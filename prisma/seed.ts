f2c6a0c2ec14cf2ce08947d253223a5959d524ff
bhuvan2205 <bhuvancs2205@gmail.com>
2025-01-15 09:41:28 +0530

    Feat: Seed script for users & products

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,12 +4,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
+  await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationRequest.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({
     data: sampleData.products,
   });
+  await prisma.user.createMany({
+    data: sampleData.users,
+  });
 
-  console.log("Database seeded successfully");
+  console.log("Database seeded successfully!");
 };
 
 main()
