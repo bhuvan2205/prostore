731e0d5242f5fb4ef35d6a29f360db83027e8593
bhuvan2205 <bhuvancs2205@gmail.com>
2025-01-15 22:05:51 +0530

    Feat: Update JWT callbacks in auth config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -55,15 +55,36 @@ export const config = {
   callbacks: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async session({ session, token, trigger, user }: any) {
-
-      // Set the user ID from the token to the session
+      // Set the user ID, role & name from the token to the session
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user }: any) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (user) {
+        token.role = user.role;
+        if (user.name === "NO_NAME") {
+          token.name = user?.email.split("@")?.at(0);
+
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              name: token.name,
+            },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
