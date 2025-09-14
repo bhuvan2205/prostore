26f6199f4661263d0434ce9b3cb3e53130515144
Bhuvan S <BhuvanS@edynamic.net>
2025-09-14 15:08:59 +0530

    Fix: Build Issue in main-nav

--- src/app/admin/_components/main-nav.tsx
+++ src/app/admin/_components/main-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ADMIN_NAVLINKS, USER_NAVLINKS } from "@/constants/user";
+import { ADMIN_NAVLINKS } from "@/constants/user";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
